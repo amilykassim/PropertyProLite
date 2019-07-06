be1794d92757cee94409b17ed632a6c07d70c7e3
amily kassim <amilykassim02@gmail.com>
2019-07-06 07:03:50 +0200

    validate user inputs

--- server/src/controllers/signin.js
+++ server/src/controllers/signin.js
@@ -4,10 +4,19 @@ import { users, generateAuthToken } from '../models/user';
 import { results, SUCCESS, ERROR } from '../helper/result';
 
 
+function validate(req) {
+  const schema = {
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  };
 
+  return Joi.validate(req.body, schema);
+}
 
 const signin = async (req, res) => {
-  res.send('sign in is working');
+  const { error } = validate(req);
+  if (error) return res.status(400).send(results(ERROR, error.details[0].message));
+
 };
 
 export default signin;
