9aedf0f48500861cf62d7582712d159e79ad230b
amily kassim <amilykassim02@gmail.com>
2019-07-06 07:03:00 +0200

    feature: initializing signin endpoint [start]

---
+++ server/src/controllers/signin.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import bcrypt from 'bcrypt';
+import { users, generateAuthToken } from '../models/user';
+import { results, SUCCESS, ERROR } from '../helper/result';
+
+
+
+
+const signin = async (req, res) => {
+  res.send('sign in is working');
+};
+
+export default signin;
