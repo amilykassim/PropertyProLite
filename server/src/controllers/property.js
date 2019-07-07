05d6e661c38ad32dd89e408a2f18a60477dc8c9f
amily kassim <amilykassim02@gmail.com>
2019-07-07 13:18:50 +0200

    [feature #167120512] fetch property of a given id

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -34,7 +34,10 @@ export const getAllProperties = (req, res) => {
 };
 
 export const getPropertyById = (req, res) => {
-  res.send('it is working as intended');
+  const property = properties.find(p => p.id === parseInt(req.params.id, 10));
+  if (!property) return res.status(404).send(results(404, 'Property with the given id does not exists'));
+
+  
 };
 
 export const postProperty = async (req, res) => {
