4388accc4d450fbdd8384d9673c8508b8beaff1a
amily kassim <amilykassim02@gmail.com>
2019-07-07 14:02:27 +0200

    [feature #167120643] fetch property with a given id

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -88,5 +88,8 @@ export const propertySold = async (req, res) => {
 };
 
 export const deleteProperty = async (req, res) => {
-  res.send('it is working perfectly');
+  let property = properties.find(p => p.id === parseInt(req.params.id, 10));
+  if (!property) return res.status(404).send(results(ERROR, 'Property with the given id does not exists'));
+
+  
 };
