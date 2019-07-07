bc77634022c60cd4402a5e007b5f5d3875620c21
amily kassim <amilykassim02@gmail.com>
2019-07-07 13:48:49 +0200

    [feature #167120606] mark property as sold

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -83,6 +83,8 @@ export const propertySold = async (req, res) => {
   const property = properties.find(p => p.id === parseInt(req.params.id, 10));
   if (!property) return res.status(404).send(results(ERROR, 'Property with the given id does not exists'));
 
+  property.status = 'sold';
+
   
 };
 
